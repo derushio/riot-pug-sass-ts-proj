417391645e7d0c3dbb6007d031f8daa357642ec1
derushio <narumi18wa@gmail.com>
2017-09-07 18:48:34 +0900

    fix

--- install-npm-script.js
+++ install-npm-script.js
@@ -8,5 +8,6 @@ json["scripts"]["build-ts"] = "rsync -a --exclude '*.ts' ./src/script/ ./src/scr
 json["scripts"]["webpack"] = "webpack --progress"
 json["scripts"]["build-pug"] = "pug --hierarchy -o ./dist/ ./src/"
 json["scripts"]["build-sass"] = "./tool/build/build-sass.sh"
+json["scripts"]["build-typedoc"] = "build-typedoc": "cd ./src; typedoc --name 'doc-web' --mode 'file' --out '../document/typedoc' './'"
 
 fs.writeFileSync("./package.json", JSON.stringify(json, null, 4));
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ npm init
 
 npm install --save riot jquery
 npm install --save-dev babel-core babel-loader babel-preset-es2015\
-        babel-preset-es2015-riot node-sass pug pug-cli riot-tag-loader typescript tsc webpack
+        babel-preset-es2015-riot node-sass pug pug-cli riot-tag-loader typescript tsc webpack typedoc
 node ./install-npm-script.js
 
 rm ./install.sh

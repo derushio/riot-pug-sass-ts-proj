95add6b11e71c313a94ea17f0f4839099eaccf4c
derushio <narumi18wa@gmail.com>
2017-09-08 17:44:19 +0900

    fix

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 npm init
 
 npm install --save riot jquery
-npm install --save-dev babel-core babel-loader babel-preset-es2015\
+npm install --save-dev babel-core babel-loader babel-preset-es2017 babel-preset-es2015\
         babel-preset-es2015-riot node-sass pug pug-cli riot-tag-loader typescript tsc webpack typedoc
 node ./install-npm-script.js
 

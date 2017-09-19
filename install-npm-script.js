e8f4ab27b3cbdba0397f8f5cdab1c64fed63b4b3
derushio <narumi18wa@gmail.com>
2017-09-19 16:56:35 +0900

    fix

--- install-npm-script.js
+++ install-npm-script.js
@@ -4,8 +4,9 @@ var json = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
 
 json["scripts"] = {}
 json["scripts"]["build"] = "node tool/build/build.js"
-json["scripts"]["build-ts"] = "rsync -a --exclude '*.ts' ./src/script/ ./src/script-es5; tsc -p src/ --outDir ./src/script-es5"
 json["scripts"]["webpack"] = "webpack --progress"
+json["scripts"]["build-ts"] = "tsc -p tmp/ --outDir ./tmp/script-es5"
+json["scripts"]["build-riot"] = "riot --config riot.config.js"
 json["scripts"]["build-pug"] = "pug --hierarchy -o ./dist/ ./src/"
 json["scripts"]["build-sass"] = "./tool/build/build-sass.sh"
 json["scripts"]["build-typedoc"] = "cd ./src; typedoc --name 'doc-web' --mode 'file' --out '../document/typedoc' './'"

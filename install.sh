6a2f497dfcbf519f87b83677aa4a3d656f0197fd
derushio <narumi18wa@gmail.com>
2017-10-09 18:06:00 +0900

    remove tsc

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ npm init
 
 npm install --save riot jquery
 npm install --save-dev babel-core babel-preset-es2017 babel-preset-es2015\
-        node-sass pug pug-cli typescript tsc webpack typedoc
+        node-sass pug pug-cli typescript webpack typedoc
 node ./install-npm-script.js
 
 rm ./install.sh

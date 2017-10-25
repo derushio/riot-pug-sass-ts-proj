2244a76d1dc2d309129018de784e47ad815c8e1a
derushio <narumi18wa@gmail.com>
2017-10-25 16:24:15 +0900

    fix babel

--- src/webpack.config.babel.js
+++ src/webpack.config.babel.js
@@ -4,10 +4,7 @@ import webpack from 'webpack'
 export default {
     context: path.join(__dirname, "../tmp/script/entry"),
     entry: {
-        "index": "./index.ts",
-        "page/sign-in": "./page/sign-in.ts",
-        "page/create-team": "./page/create-team.ts",
-        "page/edit-team": "./page/edit-team.ts"
+        "index": "./index.ts"
         // ここにエントリポイントを増やしてください
     },
     output: {

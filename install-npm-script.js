4ebfeeca3d886d010e51be8ff122ab2644666a8f
derushio <narumi18wa@gmail.com>
2017-10-25 16:29:22 +0900

    fix

--- install-npm-script.js
+++ install-npm-script.js
@@ -3,9 +3,9 @@ const fs = require('fs')
 var json = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
 
 json["scripts"] = {}
-json["scripts"]["build"] = "./scripts test_build"
-json["scripts"]["clean"] = "./scripts clean"
-json["scripts"]["clean_build"] = "./scripts clean_build"
-json["scripts"]["build_typedoc"] = "./scripts build_typedoc"
+json["scripts"]["build"] = "./scripts.sh test_build"
+json["scripts"]["clean"] = "./scripts.sh clean"
+json["scripts"]["clean_build"] = "./scripts.sh clean_build"
+json["scripts"]["build_typedoc"] = "./scripts.sh build_typedoc"
 
 fs.writeFileSync("./package.json", JSON.stringify(json, null, 4));

4349a17e6510112c67d1a67a7f5d7a34c129a185
derushio <narumi18wa@gmail.com>
2017-11-20 18:40:19 +0900

    fix

--- src/riot.config.js
+++ src/riot.config.js
@@ -15,7 +15,7 @@ export default {
     parsers: {
         css: {
             sass2: (tagName, css, opts, url) => {
-                const sassData = css.replace(/\?{(.*)?}/g, "#{$1}").replace(/(^|\n) {4,4}/g, "$1")
+                const sassData = css.replace(/\?{(.*?)}/g, "#{$1}").replace(/(^|\n) {4,4}/g, "$1")
                 const css2 = sass.renderSync({
                     data: sassData,
                     indentedSyntax: true,

0a5b9923dcdd6e4bc414e8d41aaceb46ad28415c
KID the Euforia <kid0725+github@gmail.com>
2018-01-08 14:17:29 +0900

    とりあえず動くところまで #1

--- src/webpack.config.babel.js
+++ src/webpack.config.babel.js
@@ -2,26 +2,30 @@ import path from 'path'
 import webpack from 'webpack'
 
 export default {
-    context: path.join(__dirname, "../tmp/script/entry"),
-    entry: {
-        "index": "./index.ts"
-        // ここにエントリポイントを増やしてください
-    },
-    output: {
-        path: path.join(__dirname, "../dist/script/entry"),
-        filename: "[name].js"
-    },
-    resolve: {
-        extensions: ['.ts']
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                enforce: "post",
-                loader: 'ts-loader'
-            }
-        ]
+  context: path.join(__dirname, '../tmp/script/entry'),
+  entry: {
+    'index': './index.ts'
+  },
+  output: {
+    path: path.join(__dirname, '../docs/script/entry'),
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['.ts'],
+    alias: {
+      vue: 'vue/dist/vue.js'
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        enforce: 'post',
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ]
 }
